d4068a75b4316e9c0c4090ea239639af78dc048a
Aira Domingo <airadomingo@gwu.edu>
2020-05-01 01:23:19 -0400

    commit final version

--- aira-domingo-individual-project/Code/mywork.py
+++ aira-domingo-individual-project/Code/mywork.py
@@ -1,11 +1,9 @@
 #%% ------------------------------------------- IMPORT PACKAGES --------------------------------------------------------
-
 import os
 # os.system("sudo pip install --user googledrivedownloader")
 from google_drive_downloader import GoogleDriveDownloader as gdd
 
 #%% --------------------------------------------- DOWNLOAD THE DATA ----------------------------------------------------
-
 # ID FOR DATASET'S SHARABLE LINK
 id = '0B7EVK8r0v71pZjFTYXZWM3FlRnM'
 
@@ -17,7 +15,6 @@ gdd.download_file_from_google_drive(file_id=id, dest_path=dest_path, unzip=True)
 
 
 #%% ------------------------------------------- DATA LOADER ------------------------------------------------------------
-
 def get_dataloader(batch_size, image_size, data_dir):
     transform = transforms.Compose(
         [transforms.Resize(image_size), transforms.CenterCrop(image_size), transforms.ToTensor()])
@@ -28,14 +25,12 @@ def get_dataloader(batch_size, image_size, data_dir):
 train_loader = get_dataloader(batch_size, img_size, data_dir=DATA_DIR)
 
 #%% ------------------------------------------- SCALE IMAGES -----------------------------------------------------------
-
 def scale(x, feature_range=(-1, 1)):
     min, max = feature_range
     x = x * (max - min) + min
     return x
 
 #%% ------------------------------------------- GENERATOR --------------------------------------------------------------
-# Aira
 def gen_conv(input_c, output, kernel_size, stride=2, padding=1, batch_norm=True):
     layers = []
     decon = nn.ConvTranspose2d(input_c, output, kernel_size, stride, padding, bias=False)
